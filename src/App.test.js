8377280ed74fed34f8f1b81701f94672a4b77755
Gabriel Brun <lydstyl@gmail.com>
2021-03-09 20:40:56 +0100

    dry refactor

--- src/App.test.js
+++ src/App.test.js
@@ -7,18 +7,31 @@ import App from './App';
 // set up enzyme's react adapter
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
+/**
+ * Factory function to create a ShallowWrapper for the App component.
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />)
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test="${val}"]`)
+
 test('renders without error', () => {
-  const wrapper = shallow(<App />)
-  const appComponent = wrapper.find('[data-test="component-app"]')
+  const wrapper = setup()
+  const appComponent = findByTestAttr(wrapper, 'component-app')
   expect(appComponent.length).toBe(1)
 })
 
 test('renders button', () => {
-
+  const wrapper = setup()
+  const button = findByTestAttr(wrapper, 'increment-button')
+  expect(button.length).toBe(1)
 })
 
 test('renders counter display', () => {
-
+  const wrapper = setup()
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display')
+  expect(counterDisplay.length).toBe(1)
 })
 
 test('counter starts at 0', () => {
